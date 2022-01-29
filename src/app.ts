65616759dbc7f4fb044d28bcb6e7e8c4f8be3c2d
Julien Poyard <julien.poyard@evooq.ch>
2022-01-29 18:42:52 +0100

    Game

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
-import { Game } from "phaser";
+import "phaser";
+import { GameScene } from "./gameScene";
+import { ScoreScene } from "./scoreScene";
+import { WelcomeScene } from "./welcomeScene";
 
-const config = {
-    title: "Starfall",
-    width: 800,
-    height: 600,
-    parent: "game",
-    backgroundColor: "#18216D"
+const config: Phaser.Types.Core.GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [WelcomeScene, GameScene, ScoreScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#000033"
 };
-export class StarfallGame extends Game {
+
+export class StarfallGame extends Phaser.Game {
     constructor(config) {
         super(config);
     }
